9f4b47b9b5bb6a6222aaa01025d260881a07c71e
William Poole <wpoole@caltech.edu>
2021-02-05 11:14:47 -0800

    version update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-bioscrape',
-    version='0.0.0.4',
+    version='0.0.0.5',
     packages=[
         'vivarium_bioscrape',
         'vivarium_bioscrape.processes',
