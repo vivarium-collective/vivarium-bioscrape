dbf9d31a32b7f1c22c457a1c663e814e4553b7ad
Eran Agmon <agmon.eran@gmail.com>
2021-02-11 17:45:36 -0800

    v0.0.0.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as readme:
 
 setup(
     name='vivarium-bioscrape',
-    version='0.0.0.5',
+    version='0.0.0.6',
     packages=[
         'vivarium_bioscrape',
         'vivarium_bioscrape.processes',
